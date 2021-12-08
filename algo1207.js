fb98c3d8c6583744ca98988c210bd387ddfa374a
Wenfeng(Kevin) Wu <41137645+wenfengwu@users.noreply.github.com>
2021-12-07 23:08:31 -0800

    Add files via upload

---
+++ algo1207.js
@@ -0,0 +1,92 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+// Queue
+// FIFO (First in, first out)
+
+// - enqueue
+// - dequeue
+// - peek
+// - isEmpty
+// - count
+
+class Queue {
+    constructor() {
+        this.front = null; // sometimes called head "front of the line"
+        this.back = null; // sometimes called rear or tail "back of the line"
+    }
+
+    // add nodes to the back of the queue
+    enqueue(node) {
+        if (this.back === null) { // if back is null, list is empty
+            this.back = node;
+            this.front = node;
+        } else { // otherwise add to back
+            this.back.next = node;
+            this.back = node;
+        }
+    }
+
+    // remove from the front
+    dequeue() {
+        if (this.front === null) {
+            return null; // if empty return nothing
+        };
+        if (this.front === this.back) {
+            this.back = null;
+            this.front = null;
+        };
+        let node = this.front;
+        this.front = node.next;
+        node.next = null;
+        return node;
+    }
+
+    // check the front of the queue
+    peek() {
+        return this.front ? this.front.data : this.front;
+    }
+
+    // return true / false if queue is empty
+    isEmpty() {
+        return this.front === null;
+    }
+
+    // return length
+    count() {
+        var runner = this.front;
+        var counter = 0;
+        while(runner){
+            counter++;
+            runner = runner.next;
+        }
+        return counter;
+     }
+}
+
+// NINJA BONUS:
+// print every value in the queue
+// you may only use one queue or stack for additional storage
+// return the queue back to it's original order when you are done
+// you are not allowed to linearly traverse the queue
+// only use public methods enqueue(), dequeue(), peek(), isEmpty(), and count()
+function readQueue(queue) {
+    var tempQueue = new Queue();
+
+    while (!queue.isEmpty()) {
+        // 0x017erFd214
+        var tempNode = queue.dequeue();
+        console.log(tempNode.data);
+        tempQueue.enqueue(tempNode);
+    }
+
+    while (!tempQueue.isEmpty()) {
+        queue.enqueue(tempQueue.dequeue());
+    }
+
+    return queue;
+}
